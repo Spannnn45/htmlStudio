1b479892ee8d9fc2ebf51447dae8230aa2f5ed99
Spannnn45 <62804760+Spannnn45@users.noreply.github.com>
2021-06-28 11:29:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var x = -1;
+var sum = 0;
+var dx = 1/10000000;
+
+while (x<1) {
+  sum += 2*Math.sqrt(1-x*x)*dx;
+  x += dx;
+  
+}
+
+var pi = document.getElementById("pi");
+pi.innerHTML = sum
+
